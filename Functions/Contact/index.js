ecccb846a3cb5a6c81ac8cde3c5cb11cc91cb95d
pphilip <paul.philip@nielsen.com>
2023-02-06 18:00:31 -0600

    Updated email logic

--- Functions/Contact/index.js
+++ Functions/Contact/index.js
@@ -1,10 +1,19 @@
 //General imports
 const sgMail = require('@sendgrid/mail')
+const { getFormattedDate } = require('../Helper/index.js')
+require('dotenv').config();
 
 //add message
 const sendMessage = (request, response) => {
+	let errorTemplate = {
+		error: "Send Email Error",
+		function: "sendMessage",
+		errorInfo: "",
+		timestamp: ""
+	}
+
 	var {name, contact, message} = request.body;
-	const emailList = ['paulphilip290996@gmail.com', 'paulphiliperrors@gmail.com'];
+	const emailList = ['paulphilip290996@gmail.com'];
 
 	if (!name.replace(/\s/g, "").length) {
 		name = "No Name Was Provided";
@@ -18,10 +27,10 @@ const sendMessage = (request, response) => {
 		contact = "No Contact Information Was Provided";
 	}
 
-	sgMail.setApiKey('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')
+	sgMail.setApiKey(process.env.SG_MAIL_API_KEY)
 	const msg = {
 		to: emailList,
-		from: 'paulphilipportfolio@mail.com',
+		from: 'paulphiliperrors@gmail.com',
 		subject: 'Mesage from Contact Page on Website!',
 		text: `<p>Name of Sender: ${name}<p/><p>Contact Information: ${contact}<p/><p><strong>Message:</strong><br/>${message}</p>`,
 		html: `<p>Name of Sender: ${name}<p/><p>Contact Information: ${contact}<p/><p><strong>Message:</strong><br/>${message}</p>`,
@@ -31,11 +40,17 @@ const sendMessage = (request, response) => {
 		.send(msg)
 		.then(() => {
 			console.log('Email sent')
+			response.status(200).json([`Message Sent on ${getFormattedDate()}`]);
 		})
 		.catch((error) => {
-			console.error(error)
-	})
-	response.status(201).json({status: "success", message: "Sent"});
+			const errorBlob = {
+				...errorTemplate,
+				errorInfo: error,
+				timestamp: getFormattedDate()
+			}
+			console.error(errorBlob)
+			response.status(400).json([errorBlob])
+		})
 }
 
 module.exports = { sendMessage }
